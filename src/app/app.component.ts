cc90a52493f4019df6237773c3b40419e5e35e08
YasasSri2002 <srimedagedara234@gmail.com>
2024-10-13 12:46:58 +0530

    add new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './componets/header/header.component';
+import { HomeComponent } from './componets/home/home.component';
+import { RegisterFormComponent } from './componets/register-form/register-form.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent ,RegisterFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { RegisterFormComponent } from './componets/register-form/register-form.component';
+import { HomeComponent } from './componets/home/home.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+   {
+    path : "",
+    component: HomeComponent
+   },
+    {
+        path: "home",
+        component : HomeComponent,
+    },
+    {
+        path: "register",
+        component: RegisterFormComponent
+    }
+];

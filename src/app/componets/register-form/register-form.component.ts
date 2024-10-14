630e3f2d7d163b2ca5dbb16c03e335530a271470
YasasSri2002 <srimedagedara234@gmail.com>
2024-10-14 14:37:35 +0530

    Updated

--- src/app/componets/register-form/register-form.component.ts
+++ src/app/componets/register-form/register-form.component.ts
@@ -1,46 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ReactiveFormsModule, Validators } from '@angular/forms';
 import {FormGroup, FormControl} from '@angular/forms';
-import { FormBuilder } from '@angular/forms';
 import { NgIf } from '@angular/common';
 import { Employee } from '../../model/employee.model';
-import { EmployeeService } from '../../Service/employee.service';
-import { HttpClientModule } from '@angular/common/http';
-
-
+import { HttpClient } from '@angular/common/http';
 
 
 
 @Component({
   selector: 'app-register-form',
   standalone: true,
-  imports: [ReactiveFormsModule , NgIf ,HttpClientModule ],
+  imports: [ReactiveFormsModule , NgIf  ],
   templateUrl: './register-form.component.html',
   styleUrl: './register-form.component.css'
 })
 export class RegisterFormComponent implements OnInit {
 
-  employee: Employee = {
-    empName: '',
-    age: '',
-    email: '',
-    contact: '',
-    salary : ''
-  }
-  
- public registerForm: FormGroup;
+  constructor(){}
 
-  constructor(private fb : FormBuilder,
-    private employeeService: EmployeeService
-  ){
-    this.registerForm = this.fb.group({
-      empName:['',Validators.required],
-      age:['',Validators.required],
-      email:['',[Validators.required,Validators.email]],
-      contact:['',[Validators.required,Validators.maxLength(10),Validators.minLength(10)]],
-      salary:['',Validators.required]
-    })
-  }
+  public registerForm = new FormGroup({
+    empName: new FormControl('', [Validators.required]),
+    age: new FormControl('', [Validators.required]),
+    contact: new FormControl('', [Validators.required, Validators.maxLength(10), Validators.minLength(10)]),
+    email: new FormControl('', [Validators.required,Validators.email]),
+    salary: new FormControl('', [Validators.required])
+  });
 
   ngOnInit(): void {
     
@@ -48,12 +32,8 @@ export class RegisterFormComponent implements OnInit {
 
 
   onSubmit(){
-    this.employeeService.addEmployee(this.employee).subscribe((data)=>{
-      console.log(data)
-    }
-    );
+    console.log(this.registerForm.value)
   }
-
 
 
 }
